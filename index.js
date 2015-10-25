2443fa027b486d9c3a3d1f40d11b8f9558866c52
pfan <pingfan1990@sinaapp.cn>
2015-10-25 22:41:28 +0800

    second commit

---
+++ index.js
@@ -0,0 +1,27 @@
+﻿#! /usr/bin/env node
+
+var gulp = require("gulp"),
+	child = require("child_process"),
+	shell = require("shelljs"),
+	argv = require("yargs"),
+	config = require("./package.json"),
+	gulpfile = require("./gulpfile.js");
+
+console.log("pf start！");
+
+argv.command("start", "gulp start", function (yargs) {
+console.log("Good Morning");
+gulp.start(["default"]);
+})
+.command("replace","gulp replace",function(){
+gulp.start(["replace"])
+})
+.command("hello","output hello",function(){
+	console.log("output hello")
+})
+.argv;
+
+
+	
+	
+
